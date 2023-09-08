8f1d2cc40d5c92a78c133e8cf12f963f196e0a3c
sofojedal <111465717+sofojedal@users.noreply.github.com>
2023-09-08 10:04:46 -0500

    Update Controlador-Editar Archivos.java

--- Controlador-Editar Archivos.java
+++ Controlador-Editar Archivos.java
@@ -1,17 +1,32 @@
+package Controlador;
+
+import Modelo.Editar_Modelo;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
 
+
 @WebServlet("/FileController")
-public class FileController extends HttpServlet {
-    private FileModel fileModel;
+public class Editor_Con extends HttpServlet {
+    private Editar_Modelo fileModel; // Usa el modelo FileModel para interactuar con los archivos
 
     @Override
     public void init() throws ServletException {
         super.init();
-        fileModel = new FileModel();
+        fileModel = new Editar_Modelo(); // Inicializa el modelo
     }
+    private String getFileName(Part part) {
+        String contentDisposition = part.getHeader("content-disposition");
+        String[] tokens = contentDisposition.split(";");
+        for (String token : tokens) {
+            if (token.trim().startsWith("filename")) {
+                return token.substring(token.indexOf('=') + 1).trim().replace("\"", "");
+            }
+        }
+        return "";
+    }
+
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
@@ -22,20 +37,20 @@ public class FileController extends HttpServlet {
             String fileName = getFileName(filePart);
 
             try (InputStream fileContent = filePart.getInputStream()) {
-                fileModel.uploadFile(uploadDir, fileName, fileContent);
+                fileModel.uploadFile(uploadDir, fileName, fileContent); // Llama al método del modelo
                 response.getWriter().write("Archivo subido con éxito.");
             } catch (IOException e) {
                 response.getWriter().write("Error al subir el archivo: " + e.getMessage());
             }
         } else if ("download".equals(action)) {
-            String fileName = "nombre_del_archivo_a_descargar.txt";
+            String fileName = "nombre_del_archivo_a_descargar.txt"; // Nombre del archivo a descargar (deberías obtenerlo de la solicitud)
             String filePath = getServletContext().getRealPath("/uploads/" + fileName);
 
-            File file = fileModel.getFile(filePath);
+            File file = fileModel.getFile(filePath); // Llama al método del modelo para obtener el archivo
             if (file.exists()) {
                 response.setContentType("application/octet-stream");
                 response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-                
+
                 try (InputStream fileContent = new FileInputStream(file);
                      OutputStream os = response.getOutputStream()) {
                     int bytesRead;
@@ -48,25 +63,14 @@ public class FileController extends HttpServlet {
                 response.getWriter().write("El archivo no existe.");
             }
         } else if ("delete".equals(action)) {
-            String fileName = "nombre_del_archivo_a_eliminar.txt";
+            String fileName = "nombre_del_archivo_a_eliminar.txt"; // Nombre del archivo a eliminar (deberías obtenerlo de la solicitud)
             String filePath = getServletContext().getRealPath("/uploads/" + fileName);
 
-            if (fileModel.deleteFile(filePath)) {
+            if (fileModel.deleteFile(filePath)) { // Llama al método del modelo para eliminar el archivo
                 response.getWriter().write("Archivo eliminado con éxito.");
             } else {
                 response.getWriter().write("El archivo no existe.");
             }
         }
-    }
-
-    private String getFileName(Part part) {
-        String contentDisposition = part.getHeader("content-disposition");
-        String[] tokens = contentDisposition.split(";");
-        for (String token : tokens) {
-            if (token.trim().startsWith("filename")) {
-                return token.substring(token.indexOf('=') + 1).trim().replace("\"", "");
-            }
-        }
-        return "";
     }
 }
